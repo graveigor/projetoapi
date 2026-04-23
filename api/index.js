190100ed50e91af544457ec9cb88c1d2e1e4ce30
igorgraveteixeira <igorgraveteixeira@gmail.com>
2026-04-22 22:33:24 -0300

    feat: configuraçao inicial do servidor express

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const Port = 5001;
 
+app.get("/teste",(req,res)=>{
+    res.send("Seja bem vindo ao sistema!")
+})
+
 app.listen(Port,()=>{
-    console.log(`Servidor Rodando na Porta:${Port}`)
+    console.log(`Servidor Rodando na Porta: ${Port}`)
 })
